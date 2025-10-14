3bb5749ade86e6e56d8a0687492c5aa2dbb1a6c8
Davi Cortelete <111796499+Cortelete@users.noreply.github.com>
2025-10-14 19:41:25 -0300

    Update LinkButton.tsx

--- components/LinkButton.tsx
+++ components/LinkButton.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 
 interface LinkButtonProps {
@@ -11,14 +10,14 @@ const LinkButton: React.FC<LinkButtonProps> = ({ icon, text, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className="group w-full flex items-center p-2 my-1 bg-[#3D0E0E]/80 backdrop-blur-sm border border-red-700/50 rounded-lg text-white text-sm font-semibold transition-all duration-300 ease-in-out transform hover:scale-105 hover:bg-red-800/50 hover:shadow-lg hover:shadow-red-600/50 focus:outline-none focus:ring-2 focus:ring-red-500"
+      className="relative group w-full flex items-center justify-center p-2 my-1 bg-[#3D0E0E]/80 backdrop-blur-sm border border-red-700/50 rounded-lg text-white text-sm font-semibold transition-all duration-300 ease-in-out transform hover:scale-105 hover:bg-red-800/50 hover:shadow-lg hover:shadow-red-600/50 focus:outline-none focus:ring-2 focus:ring-red-500"
     >
-      <div className="w-1/6 flex justify-start pl-2 text-gray-200 group-hover:text-white transition-colors duration-300">
+      <div className="absolute left-4 text-gray-200 group-hover:text-white transition-colors duration-300">
         {icon}
       </div>
-      <div className="flex-1 text-center">
+      <span className="text-center">
         {text}
-      </div>
+      </span>
     </button>
   );
 };
